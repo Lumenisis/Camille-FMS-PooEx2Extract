7e6834524ff2553aba339b7e003f191cf76e3a7c
Camille GASCHET <camille.gaschet1984@gmail.com>
2023-03-06 11:18:20 +0100

    Pack push

---
+++ src/TestPerson.java
@@ -0,0 +1,12 @@
+public class TestPerson {
+
+	public static void main(String[] args) {
+		
+//		Person manu = new Person ("Macron", "Emmanuel", 43, "L'Elysée à Paris", new City ("Amiens", "France"));
+//		Person sarko = new Person ("Sarkozy", "Nicolas", 66, "Paris", new City ("Paris", "France"));
+//		Person Johnson = new Person ("Johnson", "Boris", 56, "Downing Street à London", new City ("New York", "Etats-Unis"));
+//		Person gege = new Person ("Depardieu", "Gerard", 72, "Moscou", new City ("Chateauroux", "France"));
+//		Person lenny = new Person ("Kravitz", "Lenny", 56, "Hotel Particulier à Paris", new City ("New-York", "USA"));
+//		Person jenny = new Person ("Lawrence", "Jennifer", 30, "Louisville aux USA", new City ("Idan Hills", "USA"));
+	}
+}
